bc7944898d79169c7c8bae007759b3499c8c30e3
Tim Jedrek <tim@timjedrek.com>
2022-01-27 00:01:06 -0600

    create boilerplate for board and player.  move notes

---
+++ Chess/lib/board.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# creates a board graphic.  Each cell is to contain either blank or a piece
+class Board
+  def initialize
+    puts 'a board was made'
+  end
+end
--- Chess/lib/main.rb
+++ Chess/lib/main.rb
@@ -1,42 +1,18 @@
-# Flow outline
-# welcome message
-# menu
-  # new game
-  # load game
-  # save game
-  # end game
-  # resume game
-  # help
-
-# new game
-  # Human vs Human
-  # Human vs Computer
-    # make a class for player
-    # if computer, another a computer class will inherit player class and contain logic for random moves
-
-# at anytime, menu can be accessed so menu should be a "higher" class
-# for save, export board data to YAML /JSON
-# during game, player selects a piece
-# board highlights what moves are available for that peice
-# player selects the move
-# piece moves
-# peices move and get removed throughout game
-# make alert for when in check
-# need to make castle an option when available
-# once in checkmate game ends
-# or stalemate if only kings remain and 10 moves each elapses
+# frozen_string_literal: true
 
 require_relative 'main_menu'
 require_relative 'format'
+require_relative 'board'
+require_relative 'player'
 
 # Class to hold the game
 class Game
   include MainMenu
 
-  attr_accessor :player
-
   def initialize
-    @player = player
+    @board = Board.new
+    @player_one = Player.new
+    @player_two = Player.new
     main_menu
   end
 
@@ -63,7 +39,6 @@ class Game
   def help
     puts 'You need help'
   end
-
 end
 
 Game.new
